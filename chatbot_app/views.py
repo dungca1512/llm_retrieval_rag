51e016b0db1221235df8e8a5fa33623dac9b619e
dungca4 <dungca4@fpt.com>
2024-06-27 16:33:58 +0700

    Add GUI to chatbot

---
+++ chatbot_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from src.query import answer_question
+
+def chat_view(request):
+    if request.method == 'POST':
+        query = request.POST.get('query')
+        answer, context, metadatas = answer_question(query)
+        return JsonResponse({'answer': answer, 'context': context, 'metadatas': metadatas})
+    return render(request, 'chat.html')
